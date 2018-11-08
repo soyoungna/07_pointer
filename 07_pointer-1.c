3ae582f1e713213a9ad48bdba155ad61b6d0e3bf
soyoungna <hos05360@naver.com>
2018-11-08 16:17:44 +0900

    07_pointer-5

--- 07_pointer-1.c
+++ 07_pointer-1.c
@@ -4,24 +4,28 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int grade[5] = {10,20,30,40,50};
-	int average=0;
-	int *ptr;
-	
-	int i;
-	
-	//pointer setting
-	ptr=grade;
-	
-	//anverage calculation
-	for (i=0;i<5;i++)
-	{
-		 average += ptr[i];//*(ptr+i);
-	}
+	int x=10;
+	int y=20;
 	
 	
-	printf("average : %d\n", average/5);
 	
+	swap(&x,&y);
+	
+	printf("x=%d, y=%d",x,y);
+
 	
 	return 0;
+}
+void swap(int*a,int*b)
+{
+	int temp;
+	
+	temp=*a;
+	
+	*a=*b;
+	
+	*b=temp;
+	
+	return ;
+	
 }
